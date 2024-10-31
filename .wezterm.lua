5d27d4d5dfe00a0771aa57d003ea7aa5bb6d2017
gift-xipu <220123773@student.uj.ac.za>
2024-10-31 19:14:24 +0200

    commit

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,6 +9,7 @@ config = {
 	enable_tab_bar = false,
 	automatically_reload_config = true,
 	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
 	default_cursor_style = "BlinkingBar",
 	font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Regular", italic = true }),
 	font_size = 11,
@@ -26,14 +27,30 @@ config = {
 			mods = "CTRL",
 			action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
 		},
-
 		-- Switch to the next tab
 		{ key = "RightArrow", mods = "CTRL", action = wezterm.action({ ActivateTabRelative = 1 }) },
-
 		-- Switch to the previous tab
 		{ key = "LeftArrow", mods = "CTRL", action = wezterm.action({ ActivateTabRelative = -1 }) },
 	},
+	background = {
+		{
+			source = {
+				File = "/home/giftx/background/batmans.jpeg",
+			},
+			opacity = 1, -- Reduced opacity to darken the background
+			hsb = {
+				brightness = 0.1, -- Further reduce brightness
+			},
+		},
+		-- Optional: Add a dark overlay
+		{
+			source = { Color = "black" },
+			opacity = 0.5, -- Adjust this value to control darkness
+			height = "100%",
+			width = "100%",
+		},
+	},
 }
--- my coolnight colorscheme
--- and finally, return the configuration to wezterm
+
+-- Finally, return the configuration to wezterm
 return config
